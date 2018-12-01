a799b400576bd5237bc42c1de389603431f547eb
Amanda <amorilla83@gmail.com>
2018-12-01 22:30:09 +0100

    Test customer Repository

---
+++ Expenses.Core/DomainService/ICustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using Expenses.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Expenses.Core.DomainService
+{
+    public interface ICustomerRepository
+    {
+        //Create Data
+        //No Id when enter, Id when exist
+        Customer Create(Customer customer);
+        //Read Data
+        Customer ReadById(int id);
+        List<Customer> ReadAll();
+        //Update Data
+        Customer Update(Customer customerUpdate);
+        //Delete Data
+        Customer Delete(int id);
+    }
+}
