64f7490cf6d00ccc5566e16b10d04a4a72f1943f
Amanda <amorilla83@gmail.com>
2018-12-06 15:26:17 +0100

    Update to .net Core 2.2 and addScope in services

--- Expenses.API/Startup.cs
+++ Expenses.API/Startup.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Expenses.Core.ApplicationService;
+using Expenses.Core.ApplicationService.ServicesImpl;
+using Expenses.Core.DomainService;
+using Expenses.Infrastructure.Data.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -30,9 +34,15 @@ namespace Expenses.API
         public void ConfigureServices(IServiceCollection services)
         {
             //Aquí se debe añadir la configuración del log que estará definido en service extensions
-           // services.ConfigureLoggerService();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            // services.ConfigureLoggerService();
+            services.AddScoped<IStoreRepository, StoreRepository>();
+            services.AddScoped<IStoreService, StoreService>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             //services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            //Esto lo hace solo el web api pero lo pongo como ejemplo para utilizar luego unit test
+            var serviceProvider = services.BuildServiceProvider();
+            var service = serviceProvider.GetRequiredService<IStoreService>();
         }
    
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
