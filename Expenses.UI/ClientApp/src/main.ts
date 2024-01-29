0c66b259a5ed0051af4edbd188a565c45954a80e
amorilla83 <amorilla83@gmail.com>
2024-01-29 19:23:56 +0100

    Angular Standalone Step 3

--- Expenses.UI/ClientApp/src/main.ts
+++ Expenses.UI/ClientApp/src/main.ts
@@ -2,11 +2,19 @@
  * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
  */
 import '@angular/localize/init';
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { DatePipe } from '@angular/common';
 
 export function getBaseUrl() {
   return document.getElementsByTagName('base')[0].href;
@@ -20,5 +28,11 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic(providers).bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }), AppRoutingModule, FormsModule, ReactiveFormsModule, NgbModule, InfiniteScrollModule),
+        DatePipe,
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
   .catch(err => console.log(err));
